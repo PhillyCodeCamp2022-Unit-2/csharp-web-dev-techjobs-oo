e02c89995ea4289d7888c7210ae223310985f365
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-05-08 22:39:32 -0500

    Created comments/demo stuff

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -7,6 +7,10 @@ namespace TechJobsOO
         private static int nextId = 1;
 
         public string Name { get; set; }
+        // what IS EmployerName property??
+        // EmployerName is an instance of the Employer class!!
+        // EmployerName is an object from the Employer class
+        // EmployerName is an Employer object
         public Employer EmployerName { get; set; }
         public Location EmployerLocation { get; set; }
         public PositionType JobType { get; set; }
--- TechJobsOO/Program.cs
+++ TechJobsOO/Program.cs
@@ -7,9 +7,38 @@ namespace TechJobsOO
         static void Main(string[] args)
         {
             /*
+            First ToString test suggestion:
+
+            job1.ToString() should return a string that looks something like:
+
+            string testOutput = "\n" +
+            $"ID: {job1.Id}\n" +
+            "Name: Product Tester\n" +
+            "Employer: ACME\n" +
+            "Location: Desert\n" +
+            "Position Type: Quality Control\n" +
+            "Core Competency: Persistence\n";
+
+            Test this test output string against the actual output of the ToString() method, and if they match,
+            you know your ToString method's output is good
+
             Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
             Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
             Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+
+            job3.ToString() should return:
+            ID: 3
+            Name: Ice cream tester
+            Employer: Data not available
+            Location: Home
+            Position Type: UX
+            Core Competency: Tasting ability
+
+
+            // what if we wanted to make 2 jobs with the same employer?
+            Employer vmlyr = new Employer("VMLY&R");
+            Job job4 = new Job("QA Analyst", vmlyr, etc...);
+            Job job5 = new Job("Software Dev 3", vmlyr, etc...);
 
             List<Job> jobs = new List<Job>();
 
